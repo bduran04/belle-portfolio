5210e935cd37a6df4493451d4cb3ba267dc056bc
Belle Duran <bduran04@gmail.com>
2025-04-02 23:55:04 -0500

    adjusted code for build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa'
 import Image from 'next/image';
 import ProjectsList from '@/components/Projects-list';
